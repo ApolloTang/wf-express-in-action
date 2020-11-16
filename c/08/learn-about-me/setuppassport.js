1e959097962f27bb11ac4c3ba59a5d281e27b9a6
Apollo Tang <tang.apollo@gmail.com>
2020-11-16 03:13:00 -0500

    [8] add comment on passport details

--- c/08/learn-about-me/setuppassport.js
+++ c/08/learn-about-me/setuppassport.js
@@ -4,11 +4,12 @@ var LocalStrategy = require("passport-local").Strategy;
 var User = require("./models/user");
 
 module.exports = function() {
-
+  // serializeUser turns an user obj into an ID
   passport.serializeUser(function(user, done) {
     done(null, user._id);
   });
 
+  // deserializeUser turns an ID in to a user obj
   passport.deserializeUser(function(id, done) {
     User.findById(id, function(err, user) {
       done(err, user);
@@ -18,9 +19,16 @@ module.exports = function() {
   passport.use("login", new LocalStrategy(function(username, password, done) {
     User.findOne({ username: username }, function(err, user) {
       if (err) { return done(err); }
+
+      // If no user exists, then your user isn’t authenticated;
+      // say that you’ve finished with the message “No user has that username!”
       if (!user) {
         return done(null, false, { message: "No user has that username!" });
       }
+
+      // If the user does exist, compare their real password with
+      // the password you supply. If the password matches, return
+      // the current user. If it doesn’t, return “Invalid password.”
       user.checkPassword(password, function(err, isMatch) {
         if (err) { return done(err); }
         if (isMatch) {
